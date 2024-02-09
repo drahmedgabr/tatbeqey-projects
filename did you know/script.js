277f8708199d7e847bfcb388ea3bd68d47eeaf1a
drahmedgabr <ahmadal4ever@gmail.com>
2024-02-10 00:07:30 +0200

    new repo

---
+++ did you know/script.js
@@ -0,0 +1,9 @@
+var paragraphSize = 24;
+
+
+function darkMode() {
+    var body = document.body;
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+    body.style.transition = "2s";
+}
